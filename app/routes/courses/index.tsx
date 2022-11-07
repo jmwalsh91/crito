23677598d74b92513262f59a5471367e75da8b07
jmwalsh91 <jordanmwalsh@gmail.com>
2022-11-06 23:27:37 -0700

    literally center a div

--- app/routes/courses/index.tsx
+++ app/routes/courses/index.tsx
@@ -1,5 +1,5 @@
 import { Carousel } from "@mantine/carousel";
-import { Card, Title, Text, Grid, Blockquote } from "@mantine/core";
+import { Card, Title, Text, Grid, Blockquote, Container } from "@mantine/core";
 import { Outlet } from "@remix-run/react";
 import Backdrop from "~/components/Backdrop";
 import Board from "~/components/Surfaces/Board";
@@ -7,6 +7,7 @@ import Board from "~/components/Surfaces/Board";
 export default function Index() {
   return (
     <Backdrop>
+        <Container sx={{display: "flex", flexDirection: "column"}}>
         <Title order={1}>Courses.</Title>
         <Outlet />
    <Carousel
@@ -16,8 +17,8 @@ export default function Index() {
    align="start"
    slidesToScroll={2}
    sx={{
-    marginTop: "7rem",
-   }}>
+       marginTop: "7rem",
+    }}>
     <Carousel.Slide><Board title="Solargoth" image="/img1.png"/></Carousel.Slide>
     <Carousel.Slide><Board title="Phthalo" image="/img2.png"/></Carousel.Slide>
     <Carousel.Slide><Board title="Tridentata" image="/img3.png"/></Carousel.Slide>
@@ -25,12 +26,12 @@ export default function Index() {
     <Carousel.Slide><Board title="Socrates" image="/img5.png"/></Carousel.Slide>
     <Carousel.Slide><Board title="Jesuve" image="/img6.png"/></Carousel.Slide>
     <Carousel.Slide><div style={{
-      border: "1px solid black",
-     marginTop: "5rem",
-
-  
-
-
+        border: "1px solid black",
+        marginTop: "5rem",
+        
+        
+        
+        
     }}>
       <Blockquote cite="― Georges Bataille, The Unfinished System of Nonknowledge">“I think that knowledge enslaves us, that at the base of all knowledge there is a servility, the acceptation of a way of life wherein each moment has meaning only in relation to another or others that will follow it.”
 </Blockquote>
@@ -40,6 +41,7 @@ export default function Index() {
    </Carousel>
 
     <Outlet />
+        </Container> 
     </Backdrop>
      
   );
